7af587d6ab9e2b1855cb71664c915106981aa813
Касимова Софья <kasimovasofa33@gmail.com>
2025-10-06 01:26:09 +0300

    kasimova_js_kt2

--- src/tasks/task2.js
+++ src/tasks/task2.js
@@ -5,7 +5,6 @@
  * от 1 до userInput, кратных трём, через запятую с пробелом
  */
 export function getMultiplesOfThree(userInput) {
-  let result = "";
   if (userInput === null) {
     throw new Error("Пользователь отменил ввод");
   }
@@ -14,17 +13,15 @@ export function getMultiplesOfThree(userInput) {
     throw new Error("Некорректные входные данные");
   }
 
+  let result = "";
+  let a = "";
+  let i = 1;
+  while (i < userInput) {
+    i++;
+    if (i % 3 === 0) {
+      a += i + ", ";
+      result = a.slice(0, -2);
+    }
+  }
   return result;
 }
-
-let result = "";
-let a = "";
-let i = 1;
-while (i < userInput) {
-  i++;
-  if (i % 3 === 0) {
-    a += i + ", ";
-    result = a.slice(0, -2);
-  }
-}
-return result;
--- src/tasks/task3.js
+++ src/tasks/task3.js
@@ -3,37 +3,42 @@
  * @returns {string} Строка с отображением лесенки
  * @description Формирование строки с лесенкой заданной длины
  */
-const h = Number(height);
-
-if (isNaN(h)) {
-  throw new Error("Некорректные входные данные");
-}
-
-if (h < 0) {
-  throw new Error("Некорректные входные данные");
-}
-
-if (!Number.isInteger(h)) {
-  throw new Error("Некорректные входные данные");
-}
-
-if (h === 0) {
-  return "";
-}
-
-if (h === 1) {
-  return "1";
-}
-
-let result = "";
-
-for (let i = 1; i <= h; i++) {
-  for (let j = 1; j <= i; j++) {
-    result += j;
+export function renderLadder(height) {
+  if (height === null) {
+    throw new Error("Пользователь отменил ввод");
   }
 
-  if (i < h) {
-    result += "\n";
+  const h = Number(height);
+
+  if (isNaN(h)) {
+    throw new Error("Некорректные входные данные");
   }
+
+  if (h < 0) {
+    throw new Error("Некорректные входные данные");
+  }
+
+  if (!Number.isInteger(h)) {
+    throw new Error("Некорректные входные данные");
+  }
+
+  if (h === 0) {
+    return "";
+  }
+
+  if (h === 1) {
+    return "1";
+  }
+
+  let result = "";
+  for (let i = 1; i <= h; i++) {
+    for (let j = 1; j <= i; j++) {
+      result += j;
+    }
+
+    if (i < h) {
+      result += "\n";
+    }
+  }
+  return result;
 }
-return result;
